c1bd477508294d4f395c9e127a9f3fa53509bcc2
someaddons <someaddons@github.com>
2024-03-15 21:04:46 +0100

    1.5: Better dirty flag handling

--- src/main/java/com/fastasyncworldsave/mixin/SavedDataMixin.java
+++ src/main/java/com/fastasyncworldsave/mixin/SavedDataMixin.java
@@ -1,36 +1,46 @@
 package com.fastasyncworldsave.mixin;
 
-import com.fastasyncworldsave.FastAsyncWorldSave;
 import com.fastasyncworldsave.ISaveData;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.NbtIo;
 import net.minecraft.world.level.saveddata.SavedData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.io.File;
-import java.io.IOException;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Mixin(SavedData.class)
 public class SavedDataMixin implements ISaveData
 {
     @Unique
-    private CompoundTag toSave = null;
+    private final ConcurrentLinkedQueue<CompoundTag> toSave = new ConcurrentLinkedQueue<>();
 
     @Override
     public void setToSave(final CompoundTag tag)
     {
-        toSave = tag;
+        toSave.add(tag);
+    }
+
+    @Redirect(method = "save(Ljava/io/File;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/saveddata/SavedData;isDirty()Z"))
+    private boolean isSaving(final SavedData instance)
+    {
+        if (!toSave.isEmpty())
+        {
+            return true;
+        }
+        else
+        {
+            return instance.isDirty();
+        }
     }
 
     @Redirect(method = "save(Ljava/io/File;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/saveddata/SavedData;save(Lnet/minecraft/nbt/CompoundTag;)Lnet/minecraft/nbt/CompoundTag;"))
     private CompoundTag skipSer(final SavedData instance, final CompoundTag compoundTag)
     {
-        if (toSave != null)
+        if (!toSave.isEmpty())
         {
-            return compoundTag;
+            return toSave.peek();
         }
         else
         {
@@ -38,32 +48,16 @@ public class SavedDataMixin implements ISaveData
         }
     }
 
-    @Redirect(method = "save(Ljava/io/File;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/NbtIo;writeCompressed(Lnet/minecraft/nbt/CompoundTag;Ljava/io/File;)V"))
-    private void doSave(final CompoundTag compoundTag, final File file)
+    @Redirect(method = "save(Ljava/io/File;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/saveddata/SavedData;setDirty(Z)V"))
+    private void finishSaving(final SavedData instance, final boolean dirty)
     {
-        if (toSave != null)
+        if (!toSave.isEmpty())
         {
-            try
-            {
-                NbtIo.writeCompressed(toSave, file);
-            }
-            catch (IOException var4)
-            {
-                FastAsyncWorldSave.LOGGER.error("Could not save data {}", this, var4);
-            }
-
-            toSave = null;
+            toSave.poll();
         }
         else
         {
-            try
-            {
-                NbtIo.writeCompressed(compoundTag, file);
-            }
-            catch (IOException var4)
-            {
-                FastAsyncWorldSave.LOGGER.error("Could not save data {}", this, var4);
-            }
+            instance.setDirty(dirty);
         }
     }
 }
