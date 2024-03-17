be472ea3bd32717bfbe1547c1159dde38088bdb0
someaddons <someaddons@github.com>
2024-03-17 15:53:51 +0100

    1.7: Improved thread safety for async saving

--- src/main/java/com/fastasyncworldsave/mixin/SavedDataMixin.java
+++ src/main/java/com/fastasyncworldsave/mixin/SavedDataMixin.java
@@ -4,6 +4,8 @@ import com.fastasyncworldsave.ISaveData;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.saveddata.SavedData;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -13,8 +15,13 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 @Mixin(SavedData.class)
 public class SavedDataMixin implements ISaveData
 {
+    @Shadow private boolean dirty;
+
     @Unique
     private final ConcurrentLinkedQueue<CompoundTag> toSave = new ConcurrentLinkedQueue<>();
+
+    @Unique
+    private Thread currentThread = null;
 
     @Override
     public void setToSave(final CompoundTag tag)
@@ -22,10 +29,31 @@ public class SavedDataMixin implements ISaveData
         toSave.add(tag);
     }
 
+    @Override
+    public void setThread(final Thread thread)
+    {
+        currentThread = thread;
+    }
+
+    /**
+     * Dirty while saving
+     * @return
+     */
+    @Overwrite
+    public boolean isDirty()
+    {
+        return this.dirty || (currentThread != null && currentThread == Thread.currentThread());
+    }
+
+    /**
+     * Redirect dirty, incase someone overrides it
+     * @param instance
+     * @return
+     */
     @Redirect(method = "save(Ljava/io/File;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/saveddata/SavedData;isDirty()Z"))
     private boolean isSaving(final SavedData instance)
     {
-        if (!toSave.isEmpty())
+        if (currentThread != null && currentThread == Thread.currentThread())
         {
             return true;
         }
@@ -38,7 +66,7 @@ public class SavedDataMixin implements ISaveData
     @Redirect(method = "save(Ljava/io/File;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/saveddata/SavedData;save(Lnet/minecraft/nbt/CompoundTag;)Lnet/minecraft/nbt/CompoundTag;"))
     private CompoundTag skipSer(final SavedData instance, final CompoundTag compoundTag)
     {
-        if (!toSave.isEmpty())
+        if (!toSave.isEmpty() && (currentThread != null && currentThread == Thread.currentThread()))
         {
             return toSave.peek();
         }
@@ -51,7 +79,7 @@ public class SavedDataMixin implements ISaveData
     @Redirect(method = "save(Ljava/io/File;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/saveddata/SavedData;setDirty(Z)V"))
     private void finishSaving(final SavedData instance, final boolean dirty)
     {
-        if (!toSave.isEmpty())
+        if (!toSave.isEmpty() && (currentThread != null && currentThread == Thread.currentThread()))
         {
             toSave.poll();
         }
