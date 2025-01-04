a15cdcb2e898e5741767f40b93befe15c1bc52f0
someaddons <someaddons@github.com>
2025-01-04 13:56:57 +0100

    2.2: Add offthread file saving for level data

--- src/main/java/com/fastasyncworldsave/FastAsyncWorldSave.java
+++ src/main/java/com/fastasyncworldsave/FastAsyncWorldSave.java
@@ -9,6 +9,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 import static com.fastasyncworldsave.FastAsyncWorldSave.MOD_ID;
 
@@ -20,6 +23,17 @@ public class FastAsyncWorldSave
     public static final Logger LOGGER = LogManager.getLogger();
     //private static      CupboardConfig<CommonConfiguration> config = new CupboardConfig<>(MOD_ID, new CommonConfiguration());
     public static       Random rand   = new Random();
+
+    public static ExecutorService threadPool = Executors.newSingleThreadExecutor(new ThreadFactory()
+    {
+        @Override
+        public Thread newThread(final Runnable r)
+        {
+            Thread thread = new Thread(r);
+            thread.setName(MOD_ID);
+            return thread;
+        }
+    });
 
     public FastAsyncWorldSave(IEventBus modEventBus, ModContainer modContainer)
     {
---
+++ src/main/java/com/fastasyncworldsave/mixin/LevelStorageSourceMixin.java
@@ -0,0 +1,47 @@
+package com.fastasyncworldsave.mixin;
+
+import com.fastasyncworldsave.FastAsyncWorldSave;
+import net.minecraft.Util;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtIo;
+import net.minecraft.world.level.storage.LevelStorageSource;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Mixin(LevelStorageSource.LevelStorageAccess.class)
+public class LevelStorageSourceMixin
+{
+    @Shadow
+    @Final
+    public LevelStorageSource.LevelDirectory levelDirectory;
+
+    /**
+     * @author sam
+     * @reason offthread saving
+     */
+    @Overwrite
+    private void saveLevelData(CompoundTag compoundtag1)
+    {
+        FastAsyncWorldSave.threadPool.submit(() -> {
+            Path path = this.levelDirectory.path();
+
+            try
+            {
+                Path path1 = Files.createTempFile(path, "level", ".dat");
+                NbtIo.writeCompressed(compoundtag1, path1);
+                Path path2 = this.levelDirectory.oldDataFile();
+                Path path3 = this.levelDirectory.dataFile();
+                Util.safeReplaceFile(path3, path1, path2);
+            }
+            catch (Exception e)
+            {
+                FastAsyncWorldSave.LOGGER.error("Failed to save level {} data:" + compoundtag1, path, e);
+            }
+        });
+    }
+}
