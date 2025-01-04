41cc4dcd751478b86cff85c394b4498099b65521
someaddons <someaddons@github.com>
2025-01-04 13:56:57 +0100

    2.2: Add offthread file saving for level data

--- src/main/java/com/fastasyncworldsave/FastAsyncWorldSave.java
+++ src/main/java/com/fastasyncworldsave/FastAsyncWorldSave.java
@@ -4,8 +4,12 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.resources.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 public class FastAsyncWorldSave implements ModInitializer
 {
@@ -14,6 +18,17 @@ public class FastAsyncWorldSave implements ModInitializer
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
     //private static CupboardConfig<CommonConfiguration> config = new CupboardConfig<>(MOD_ID, new CommonConfiguration());
     public static       Random rand   = new Random();
+
+    public static ExecutorService threadPool = Executors.newSingleThreadExecutor(new ThreadFactory()
+    {
+        @Override
+        public Thread newThread(@NotNull final Runnable r)
+        {
+            Thread thread = new Thread(r);
+            thread.setName(MOD_ID);
+            return thread;
+        }
+    });
 
     @Override
     public void onInitialize()
--- src/main/java/com/fastasyncworldsave/mixin/DimensionDataStorageMixin.java
+++ src/main/java/com/fastasyncworldsave/mixin/DimensionDataStorageMixin.java
@@ -1,7 +1,6 @@
 package com.fastasyncworldsave.mixin;
 
 import com.fastasyncworldsave.FastAsyncWorldSave;
-import net.minecraft.Util;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtIo;
 import net.minecraft.nbt.NbtUtils;
@@ -69,7 +68,7 @@ public abstract class DimensionDataStorageMixin
         if (toSave != null)
         {
             final Map<String, CompoundTag> saveData = toSave;
-            Util.ioPool().submit(() -> {
+            FastAsyncWorldSave.threadPool.submit(() -> {
                 for (final var toSaveEntry : saveData.entrySet())
                 {
                     try
