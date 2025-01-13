e5f20b2e7f912048b10f7db277a37a7bf9aee442
someaddons <someaddons@github.com>
2025-01-13 13:11:19 +0100

    2.3: Fix thread not shutting down on server close

--- src/main/java/com/fastasyncworldsave/FastAsyncWorldSave.java
+++ src/main/java/com/fastasyncworldsave/FastAsyncWorldSave.java
@@ -1,6 +1,7 @@
 package com.fastasyncworldsave;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.resources.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -33,6 +34,12 @@ public class FastAsyncWorldSave implements ModInitializer
     @Override
     public void onInitialize()
     {
+        ServerLifecycleEvents.SERVER_STOPPED.register(s -> {
+            if (s.isDedicatedServer())
+            {
+                threadPool.shutdown();
+            }
+        });
     }
 
     public static ResourceLocation id(String name)
