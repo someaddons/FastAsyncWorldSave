4215086f812eb628ad333bec16887535c3e8c64c
someaddons <someaddons@github.com>
2025-08-09 23:26:55 +0200

    2.5: Add player data to async save, removed world data as default is now async

---
+++ src/main/java/com/fastasyncworldsave/mixin/PlayerDatStorageMixin.java
@@ -0,0 +1,50 @@
+package com.fastasyncworldsave.mixin;
+
+import com.fastasyncworldsave.FastAsyncWorldSave;
+import net.minecraft.Util;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtIo;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.storage.PlayerDataStorage;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static com.fastasyncworldsave.FastAsyncWorldSave.LOGGER;
+
+@Mixin(PlayerDataStorage.class)
+public class PlayerDatStorageMixin
+{
+    @Redirect(method = "save", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/NbtIo;writeCompressed(Lnet/minecraft/nbt/CompoundTag;Ljava/nio/file/Path;)V"))
+    private void writeAsync(final CompoundTag compoundTag, final Path path, final Player player)
+    {
+        FastAsyncWorldSave.threadPool.submit(() -> {
+            try
+            {
+                NbtIo.writeCompressed(compoundTag, path);
+            }
+            catch (IOException e)
+            {
+                LOGGER.warn("Failed to save player data for {}", player.getName().getString());
+            }
+        });
+    }
+
+    @Redirect(method = "save", at = @At(value = "INVOKE", target = "Lnet/minecraft/Util;safeReplaceFile(Ljava/nio/file/Path;Ljava/nio/file/Path;Ljava/nio/file/Path;)V"))
+    private void writeAsync(final Path path3, final Path path2, final Path path4, final Player player)
+    {
+        FastAsyncWorldSave.threadPool.submit(() -> {
+            try
+            {
+                Util.safeReplaceFile(path3, path2, path4);
+            }
+            catch (Exception e)
+            {
+                LOGGER.warn("Failed to save player data for {}", player.getName().getString());
+            }
+        });
+    }
+}
