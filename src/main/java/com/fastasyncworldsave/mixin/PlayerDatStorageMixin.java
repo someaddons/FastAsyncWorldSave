fec6e9c36f64b2f1a6e06853425b1c9c3c39bbb2
someaddons <someaddons@github.com>
2026-04-15 16:06:31 +0200

    26.1

--- src/main/java/com/fastasyncworldsave/mixin/PlayerDatStorageMixin.java
+++ src/main/java/com/fastasyncworldsave/mixin/PlayerDatStorageMixin.java
@@ -1,9 +1,9 @@
 package com.fastasyncworldsave.mixin;
 
 import com.fastasyncworldsave.FastAsyncWorldSave;
-import net.minecraft.Util;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtIo;
+import net.minecraft.util.Util;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.storage.PlayerDataStorage;
 import org.spongepowered.asm.mixin.Mixin;
@@ -33,7 +33,7 @@ public class PlayerDatStorageMixin
         });
     }
 
-    @Redirect(method = "save", at = @At(value = "INVOKE", target = "Lnet/minecraft/Util;safeReplaceFile(Ljava/nio/file/Path;Ljava/nio/file/Path;Ljava/nio/file/Path;)V"))
+    @Redirect(method = "save", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Util;safeReplaceFile(Ljava/nio/file/Path;Ljava/nio/file/Path;Ljava/nio/file/Path;)V"))
     private void writeAsync(final Path path3, final Path path2, final Path path4, final Player player)
     {
         FastAsyncWorldSave.threadPool.submit(() -> {
